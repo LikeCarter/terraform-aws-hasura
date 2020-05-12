57e0f4eff14d7d3fcb99d3c048f33219e7f5b860
Carter Sprigings <carter@sprigings.com>
2020-05-12 17:34:25 -0400

    Change ALB sec. groups.

--- main.tf
+++ main.tf
@@ -73,6 +73,13 @@ resource "aws_security_group" "hasura_alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
